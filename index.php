b5ae851c6ffbef23cbb3b480fe4e3c23679f796f
Rondeo Balos <47505842+rondeo-balos@users.noreply.github.com>
2021-05-23 17:28:13 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <!doctype html>
-<title>JGRAN Server</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width">
-<link rel="stylesheet" type="text/css" href="https://ajax.aspnetcdn.com/ajax/bootstrap/4.3.1/css/bootstrap.min.css">
+<head>
+	<title>JGRAN Server</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" type="text/css" href="https://ajax.aspnetcdn.com/ajax/bootstrap/4.3.1/css/bootstrap.min.css">
+	<script data-ad-client="ca-pub-9337117132091940" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+</head
 
 <body style="background-repeat: no-repeat; background-attachment: fixed; background-position: center; background-size: cover; background-image: url(https://i.postimg.cc/DwmY9nBf/wallpaper.jpg);">
 	<br><br><br><br>

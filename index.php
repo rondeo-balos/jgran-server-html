c1128248f54c625ffa0936fa6890e030816056f1
Rondeo Balos <47505842+rondeo-balos@users.noreply.github.com>
2021-05-25 08:43:55 +0800

    update hyperlink

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
 					</tr>
 					<tr>
 						<th>password:</th>
-						<td> <a href="http://q.gs/FNs7h">Join Discord</a></td>
+						<td> <a href="http://q.gs/FNs7h">Join Discord</a> to get the password</td>
 					</tr>
 				</table>
 
